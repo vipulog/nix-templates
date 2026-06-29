e69fca2ca1703e74f9d04b05cbe7f17035bdad9d
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2026-06-29 18:46:53 +0530

    style: reformat treefmt module structure

--- simple/flake-parts/treefmt.nix
+++ simple/flake-parts/treefmt.nix
@@ -1,14 +1,16 @@
 {inputs, ...}: {
   imports = [inputs.treefmt-nix.flakeModule];
 
-  perSystem.treefmt = {
-    programs = {
-      alejandra.enable = true;
-      deadnix.enable = true;
-    };
+  perSystem = {
+    treefmt = {
+      programs = {
+        alejandra.enable = true;
+        deadnix.enable = true;
+      };
 
-    settings.formatter = {
-      deadnix.no_lambda_arg = true;
+      settings.formatter = {
+        deadnix.no_lambda_arg = true;
+      };
     };
   };
 }
